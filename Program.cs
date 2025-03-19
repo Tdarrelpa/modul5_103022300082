9832cf03087462dedfc3edf9e39bc15226b0f620
Tdarrelpa <darrel2208@gmail.com>
2025-03-19 07:51:27 +0700

    menambahkan class PemrosesData dengan method DapatkanNilaiTerbesar()

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using cobaProsesData;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        PemrosesData pemrosesData = new PemrosesData();
+        float hasil = pemrosesData.DapatkanNilaiTerbesar<float>(10, 30, 82);
+        Console.WriteLine($"Largest data is {hasil}");
+    }
+}
---
+++ cobaProsesData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cobaProsesData 
+{
+    public class PemrosesData 
+    {   
+        public T DapatkanNilaiTerbesar<T>(T data1, T data2, T data3)
+        {
+            dynamic temp1 = data1;
+            dynamic temp2 = data2;
+            dynamic temp3 = data3;
+
+            dynamic? Largest = temp1;
+            if (Largest <= temp2)
+            {
+                Largest = temp2;
+            }
+
+            if (Largest <= temp3)
+            {
+                Largest = temp3;
+            }
+            return Largest;
+        }
+    }
+}
