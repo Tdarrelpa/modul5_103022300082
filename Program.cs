de6cca81b0629e66baa0076294d7574b747b1b45
Tdarrelpa <darrel2208@gmail.com>
2025-03-19 07:44:11 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more information\
+using cobaBasisData;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<int> db = new SimpleDataBase<int>();
+        db.AddData(12);
+        db.AddData(34);
+        db.AddData(56);
+        db.ShowData();
+    }
+}
---
+++ cobaBasisData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace cobaBasisData
+{
+    public class SimpleDataBase<T> 
+    {
+        private List<T> Storeddata;
+        private List<DateTime> inputDate;
+
+        public SimpleDataBase()
+        {
+            Storeddata = new List<T>();
+            inputDate = new List<DateTime>();
+        }
+
+        public void AddData(T data)
+        {
+            this.Storeddata.Add(data);
+            this.inputDate.Add(DateTime.Now);
+        }
+
+        public void ShowData()
+        {
+            for (int i = 0; i < Storeddata.Count; i++)
+            {
+                Console.WriteLine("Data " + i+1 + "berisi: " + Storeddata[i] + ", yang disimpan pada waktu UTC: " + inputDate[i]);
+            }
+        }
+    }
+}
